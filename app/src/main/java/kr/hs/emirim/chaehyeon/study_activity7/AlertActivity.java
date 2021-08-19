24333068df0fc1a5b708561c83107da956447ff2
chaehyeon7 <81363193+chaehyeon7@users.noreply.github.com>
2021-08-19 14:52:06 +0900

    Edit Dialog(Add AlertActivity.java) Commit

---
+++ app/src/main/java/kr/hs/emirim/chaehyeon/study_activity7/AlertActivity.java
@@ -0,0 +1,50 @@
+package kr.hs.emirim.chaehyeon.study_activity7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class AlertActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alert);
+    }//onCreate()
+
+    @Override
+    public void onBackPressed() {
+
+        //AlertDialog 생성
+        AlertDialog.Builder dialog = new AlertDialog.Builder( AlertActivity.this);
+
+        dialog.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+
+        dialog.setNegativeButton("no", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(AlertActivity.this, "no click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        dialog.setNeutralButton("neutral", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(AlertActivity.this, "neutral click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        dialog.setTitle("app name");
+        dialog.setMessage("are you want to exit application?");
+        dialog.show();
+
+    }
+}
